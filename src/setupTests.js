0b40f62be69dafbc0e5e51af745fde68553b3273
Brandon864 <ojbrandon2345@mail.com>
2025-04-25 22:55:12 +0300

    Update: npm

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// Import jest-dom for custom matchers like toBeInTheDocument
+import "@testing-library/jest-dom";
+
+// Enable fetch mocking
+import fetchMock from "jest-fetch-mock";
+fetchMock.enableMocks();w
